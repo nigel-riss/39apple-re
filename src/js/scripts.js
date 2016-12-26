0c4fdb63f928074db81cab0d55c9ccfb45dcd3ac
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2016-12-26 08:15:17 +0200

    Moved Header and Footer to seperate file. Catalog started

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -18,19 +18,19 @@ var granimInstance = new Granim({
     }
 });
 
-var granimInstance = new Granim({
-    element: '.main-nav__canvas',
-    name: 'basic-gradient',
-    direction: 'radial',
-    opacity: [1, 1],
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#04b004', '#2ecc71'],
-                ['#2ecc71', '#04b004'],
-                ['#144258', '#024c45']
-            ]
-        }
-    }
-});
+// var granimInstance = new Granim({
+//     element: '.main-nav__canvas',
+//     name: 'basic-gradient',
+//     direction: 'radial',
+//     opacity: [1, 1],
+//     isPausedWhenNotInView: true,
+//     states : {
+//         "default-state": {
+//             gradients: [
+//                 ['#04b004', '#2ecc71'],
+//                 ['#2ecc71', '#04b004'],
+//                 ['#144258', '#024c45']
+//             ]
+//         }
+//     }
+// });
