63662ca809a23abd4169f01a3fe2aef87fa42c56
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2016-12-30 19:09:15 +0200

    Footer finished, About page pinished, News-item fix

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -34,3 +34,38 @@ var granimInstance = new Granim({
 //         }
 //     }
 // });
+
+// Создаем обработчик загрузки страницы:  
+DG.autoload(function () {
+    // Создаем объект карты, связанный с контейнером:  
+    var myMap = new DG.Map('myMapId');
+    // Устанавливаем центр карты, и коэффициент масштабирования: javascript:// 
+    myMap.setCenter(new DG.GeoPoint(20.512794, 54.713147), 15);
+    // Добавляем элемент управления коэффициентом масштабирования:  
+    myMap.controls.add(new DG.Controls.Zoom());
+
+    // Создаем балун: 
+    var myBalloon = new DG.Balloons.Common({
+        // Местоположение на которое указывает балун:  
+        geoPoint: new DG.GeoPoint(20.512794, 54.713147),
+        // Устанавливаем текст, который будет отображатся при открытии балуна: 
+        contentHtml: '39 Яблок & Смартфонов'
+    });
+    // Создаем маркер: 
+    var myMarker = new DG.Markers.Common({
+        // Местоположение на которое указывает маркер: 
+        geoPoint: new DG.GeoPoint(20.512794, 54.713147),
+        // Функция, вызываемая при клике по маркеру 
+        clickCallback: function () {
+            if (!myMap.balloons.getDefaultGroup().contains(myBalloon)) {
+                // Если балун еще не был добавлен на карту, добавляем его: 
+                myMap.balloons.add(myBalloon);
+            } else {
+                // Показываем уже ранее добавленный на карту балун 
+                myBalloon.show();
+            }
+        }
+    });
+    // Добавить маркер на карту: 
+    myMap.markers.add(myMarker);
+});
