f67fcd14314a4cb53dbbc0d6cf57eddac2513e37
Yuriy Kurenkov <mr.kurenkov@gmail.com>
2017-01-03 20:01:06 +0200

    Header and Footer mobile and Tablet portrait finished

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,22 +1,22 @@
 ;
 console.log('hi');
 
-var granimInstance = new Granim({
-    element: '.page-header__canvas',
-    name: 'basic-gradient',
-    direction: 'radial',
-    opacity: [1, 1],
-    isPausedWhenNotInView: true,
-    states : {
-        "default-state": {
-            gradients: [
-                ['#04b004', '#2ecc71'],
-                ['#2ecc71', '#04b004'],
-                ['#144258', '#024c45']
-            ]
-        }
-    }
-});
+// var granimInstance = new Granim({
+//     element: '.page-header__canvas',
+//     name: 'basic-gradient',
+//     direction: 'radial',
+//     opacity: [1, 1],
+//     isPausedWhenNotInView: true,
+//     states : {
+//         "default-state": {
+//             gradients: [
+//                 ['#04b004', '#2ecc71'],
+//                 ['#2ecc71', '#04b004'],
+//                 ['#144258', '#024c45']
+//             ]
+//         }
+//     }
+// });
 
 // var granimInstance = new Granim({
 //     element: '.main-nav__canvas',
